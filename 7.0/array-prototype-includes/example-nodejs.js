6f924ebc6369238df7dd51ecdd96b7859dba2711
arcticoder <arcticoder@caramail.com>
2016-10-16 23:35:47 -0700

    example - 7.0 - Array.prototype.includes

---
+++ 7.0/array-prototype-includes/example-nodejs.js
@@ -0,0 +1,6 @@
+var myArray = [1, 2, 3, 4, 5];
+
+// Convert the following index check from indexOf style to use Array.prototype.includes:
+if (myArray.indexOf(3) !== -1) {
+    console.log("myArray contains a 3");
+}
---
+++ 7.0/array-prototype-includes/solution/solution-nodejs.js
@@ -0,0 +1,5 @@
+var myArray = [1, 2, 3, 4, 5];
+
+if (myArray.includes(3)) {
+    console.log("myArray contains a 3");
+}
